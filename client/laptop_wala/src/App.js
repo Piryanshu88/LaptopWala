9ad3d850640fd55bd280eaafa91c946b1a102bbf
Piryanshu88 <deepubisht2004@gmail.com>
2023-01-17 19:13:34 +0530

    adding react folder

--- client/laptop_wala/src/App.js
+++ client/laptop_wala/src/App.js
@@ -1,10 +1,14 @@
-import logo from "./logo.svg";
+import { Text } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return (
+    <div className="App">
+      <Text fontSize={"4xl"} textAlign="center">
+        Jai Mata di
+      </Text>
+    </div>
+  );
 }
 
 export default App;
