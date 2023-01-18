1e38b8026e2dd27f9a68fa2676a0f6bdb9ddd872
Piryanshu88 <deepubisht2004@gmail.com>
2023-01-19 01:59:50 +0530

    adding models for users

---
+++ server/model/user.model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const reqString = { type: String, required: true };
+
+const userSchema = mongoose.Schema({
+  firstName: reqString,
+  lastName: reqString,
+  email: reqString,
+  password: reqString,
+});
+
+const UserModel = mongoose.model("user", userSchema);
