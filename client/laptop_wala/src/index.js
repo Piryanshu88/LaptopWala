0384ecea845c760a61755a23f32401104bc5cc22
Piryanshu88 <deepubisht2004@gmail.com>
2023-01-21 03:29:08 +0530

    adding redux store

--- client/laptop_wala/src/index.js
+++ client/laptop_wala/src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Provider as ReduxProvider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider>
     {/* <ReduxProvider> */}
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
     {/* </ReduxProvider> */}
   </ChakraProvider>
